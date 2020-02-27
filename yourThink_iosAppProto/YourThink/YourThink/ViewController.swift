a1f4e5dde145315a71de3c75bf8d6b8f84d220b2
misseop25 <richard25@naver.com>
2020-02-27 20:01:10 +0900

    빈 프로젝트 생성

---
+++ yourThink_iosAppProto/YourThink/YourThink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourThink
+//
+//  Created by Minseop Kim on 2020/02/27.
+//  Copyright © 2020 yourThink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
